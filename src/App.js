329a33e85f658f44002de5e8a1d59159db8116a1
FowziaSultana <fowziasultana168@gmail.com>
2023-03-31 01:06:44 +0600

    first commit navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import Header from "./Header/Header";
 
 function App() {
+  // const [blogs, setBlogs] = useState([]);
+  // useEffect(() => {
+  //   fetch("fakeData.json")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-body-tertiary navbar-expand-lg">
+      <div className="container">
+        <h4>Knowledge Cafe </h4>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+            <li className="nav-item">
+              <span className="nav-link">Home</span>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link">Contacts</span>
+            </li>
+            <li className="nav-item ">
+              <span className="nav-link">About</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
