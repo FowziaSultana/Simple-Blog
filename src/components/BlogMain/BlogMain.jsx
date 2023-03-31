504bb79b81c3c102b58bc3340f9c97aa81b16824
FowziaSultana <fowziasultana168@gmail.com>
2023-03-31 20:37:55 +0600

    added Marked button and Functionality of it

--- src/components/BlogMain/BlogMain.jsx
+++ src/components/BlogMain/BlogMain.jsx
@@ -3,22 +3,39 @@ import "./BlogMain.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import SingleBlog from "../SingleBlog/SingleBlog";
+import SideBar from "../SideBar/SideBar";
 
 const BlogMain = () => {
+  const [markedBlogs, setMarkBlog] = useState([]);
+
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     fetch("fakeData.json")
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
+  const handleMarkRead = (title) => {
+    let tempArr = [...markedBlogs, title];
+    setMarkBlog(tempArr);
+  };
+
   return (
-    <div className="main-container container row">
+    <div className="container row mx-auto mt-5 ">
       <div className="col-lg-8">
         {blogs.map((aBlog) => (
-          <SingleBlog aBlog={aBlog} key={aBlog.id}></SingleBlog>
+          <SingleBlog
+            aBlog={aBlog}
+            key={aBlog.id}
+            handleMarkRead={handleMarkRead}
+          ></SingleBlog>
         ))}
       </div>
-      <div className="col-lg-4">Side BAr</div>
+      <div className="col-lg-4">
+        <h1>Book Marked Blogs :{markedBlogs.length}</h1>
+        {markedBlogs.map((aMark) => (
+          <SideBar aMark={aMark}></SideBar>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -11,9 +11,25 @@ const SingleBlog = (props) => {
     readTime,
     publishDate,
   } = props.aBlog;
+  const handleMarkRead = props.handleMarkRead;
   return (
-    <div>
-      <img src={authorImages}></img>
+    <div className="card mb-3s">
+      {/* <img src={blogCoverImages} className="card-img-top" alt="..." /> */}
+      <div className="card-body">
+        <h5 className="card-title text-start">{blogTitle}</h5>
+        <p className="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+        <p className="card-text">
+          <button
+            className="btn btn-info"
+            onClick={() => handleMarkRead(blogTitle)}
+          >
+            Mark as red
+          </button>
+        </p>
+      </div>
     </div>
   );
 };
